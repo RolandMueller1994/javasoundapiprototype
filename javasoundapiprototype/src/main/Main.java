5f2ac49a9391308e7f42fa059b8ea5e32f3609cb
Kone <Kone@Kone-Notebook.fritz.box>
2017-06-01 18:27:56 +0200

    Test Kone

--- javasoundapiprototype/src/main/Main.java
+++ javasoundapiprototype/src/main/Main.java
@@ -1,10 +1,18 @@
 package main;
 
+/**
+ * 
+ * @author Kone
+ *
+ */
 public class Main {
 
+	/**
+	 * kldöaäsdflöä
+	 * @param args jklasdf
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
